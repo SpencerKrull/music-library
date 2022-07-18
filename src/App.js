61af4713458dc557f146ac7f7d912ab2c9ac78cd
Spencer Krull <spencerkrull138@gmail.com>
2022-07-17 18:19:19 -0600

    merged all 3 to the best of my ability

--- src/App.js
+++ src/App.js
@@ -1,83 +1,62 @@
-import { useEffect, useState, useRef, Fragment, Suspense } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { createResource as fetchData } from './helper'
-import Gallery from './Components/Gallery'
-import SearchBar from './Components/SearchBar'
-import AlbumView from './Components/AlbumView'
-import ArtistView from './Components/ArtistView'
-import { DataContext } from './Context/DataContext'
-import { SearchContext } from './Context/SearchContext'
-import Spinner from './Components/Spinner'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Gallery from "./Components/Gallery";
+import SearchBar from "./Components/SearchBar";
+import AlbumView from "./Components/AlbumView";
+import ArtistView from "./Components/ArtistView";
+import "./App.css";
+import { useState, Suspense, useEffect } from "react";
+import Spinner from "./Components/Spinner";
+import { createResource as fetchData } from "./helper";
 
 function App() {
-	let [searchTerm, setSearchTerm] = useState('')
-	let [message, setMessage] = useState('Search for Music!')
-	let [data, setData] = useState(null)
-  let searchInput = useRef('')
-
-	const API_URL = 'https://itunes.apple.com/search?term='
+  let [searchTerm, setSearchTerm] = useState("");
+  let [message, setMessage] = useState("Search for Music!");
+  let [data, setData] = useState(null);
 
   useEffect(() => {
     if (searchTerm) {
-      document.title=`${searchTerm} Music`
-      console.log(fetchData(searchTerm))
-      setData(fetchData(searchTerm))
-  }
-  }, [searchTerm])
-	
-	const handleSearch = (e, term) => {
-		e.preventDefault()
-		setSearchTerm(term)
-	}
+      document.title = `${searchTerm} Music`;
+      console.log(fetchData(searchTerm));
+      setData(fetchData(searchTerm));
+    }
+  }, [searchTerm]);
 
-	return (
-		<div>
-			{message}
-			<Router>
-				<Routes>
-					<Route path="/" element={
-						<Fragment>
-							<SearchBar handleSearch = {handleSearch}/>
-							<Gallery data={data} />
-						</Fragment>
-					} />
-					<Route path="/album/:id" element={<AlbumView />} />
-					<Route path="/artist/:id" element={<ArtistView />} />
-				</Routes>
-			</Router>
-		</div>
-  	);
+  const handleSearch = (e, term) => {
+    e.preventDefault();
+    setSearchTerm(term);
+  };
 
-  return (
-    <div className="App">
-      <SearchContext.Provider value={{term: searchInput, handleSearch: handleSearch}}>
-        <SearchBar />
-      </SearchContext.Provider>
-      {message}
-      <DataContext.Provider value={data}>
-        <Gallery />
-      </DataContext.Provider>
-    </div>
-  );
   const renderGallery = () => {
-    if(data){
+    if (data) {
       return (
         <Suspense fallback={<Spinner />}>
           <Gallery data={data} />
         </Suspense>
-      )
+      );
     }
-  }
+  };
 
   return (
-    <div className="App">
-        <SearchBar handleSearch={handleSearch} />
-        {message}
-        {renderGallery()}
+    <div>
+      {message}
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchBar handleSearch={handleSearch} />
+                {/* <Gallery data={data} /> */}
+                {renderGallery()}
+              </>
+            }
+          />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </Router>
     </div>
-)
-
-
+  );
 }
 
 export default App;
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,15 +1,19 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { SearchContext } from '../Context/SearchContext'
 
-const SearchBar = () => {
-    const {term, handleSearch} = useContext(SearchContext)
-
+const SearchBar = (props) => {
+    let [searchTerm, setSearchTerm] = useState("");
+  
     return (
-            <form>
-                <input ref={term} type="text" placeholder="Search Here" />
-                <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
-            </form>
-    )
-}
-
-export default SearchBar
+      <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+        <input
+          type="text"
+          placeholder="Search Here"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <input type="submit" />
+      </form>
+    );
+  };
+  
+  export default SearchBar;
